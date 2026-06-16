f4e82f9873908b4224c7282d1b88078f2dfeac50
Hamza Osama <hamzaosamaa12@gmail.com>
2026-06-16 13:45:00 +0300

    endpoint update

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(BaseModel):
    email: Annotated[str, Indexed()]
    age: int
    disabled: bool = False
+   account_level: int = Field(ge=1, le=4) #client coach staff admin
 
 class UserInDB(Document, User):
    hashed_password: str #we create this so we dont send passwords in API responses as its super unsafe
